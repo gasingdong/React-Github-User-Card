93aa05aedf806a62d695b51baadc43704602c112
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-06 11:47:40 -0700

    create typescript react app

---
+++ github-user-card/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  "parser":  "@typescript-eslint/parser",
+  "extends": [
+	"plugin:react/recommended",
+	"plugin:@typescript-eslint/recommended",
+	"prettier/@typescript-eslint",
+	"plugin:prettier/recommended",
+	],
+  "parserOptions": {
+	"ecmaVersion": 2018,
+	"sourceType": "module",
+	"ecmaFeatures": {
+      "jsx": true
+    }
+  },
+  "env": { 
+    "es6": true,
+  },
+  "globals": {
+    "console": "readonly",
+	"document": "readonly",
+	"window": "readonly",
+  },
+  "settings": {
+	"react": {
+	  "version": 'detect',
+	}
+  }
+};
